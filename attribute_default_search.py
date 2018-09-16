2f8e3c8908db290e180b43748f97300dec260a5c
Foo2018 <chriswhiting6868@live.com>
2018-09-17 00:15:12 +1200

    Create new class to handle searching for attribute defaults

---
+++ attribute_default_search.py
@@ -0,0 +1,59 @@
+import re
+
+
+class AttributeDefaultsSearch(object):
+    def __init__(self):
+        self.def_data_types_dict = None
+
+    def attribute_extraction(self, line):
+        return self._extract_defaults_data_types(line)
+
+    @staticmethod
+    def _regex_search(regex, data):
+        r = re.compile(regex)
+        regex_result = r.findall(data)
+        return regex_result
+
+    def _extract_defaults_data_types(self, line):
+        attr_default = self._extract_attribute_defaults(line)
+        if not attr_default:
+            return ''
+        else:
+            attr_type = self._extract_attribute_data_types(attr_default)
+            def_data_types_dict = {attr_type: attr_default}
+            return def_data_types_dict
+
+    # Extracts default value(s) from an attribute and returns them
+    def _extract_attribute_defaults(self, line):
+        regex = '\s{2}self\.\w+\s=\s(.*)'
+        attribute_default = self._regex_search(regex, line)
+        if attribute_default.__len__() != 0:
+            attribute_default = attribute_default[0].replace('"', "")
+        return attribute_default
+
+    # Identifies attribute data type returns it
+    def _extract_attribute_data_types(self, attr_name):
+        regex = '^(.)'
+        regex2 = '^[A-Z].+\)$'
+        extracted_type = self._regex_search(regex, attr_name)
+        data_type = extracted_type[0]
+        extracted_type_2 = self._regex_search(regex2, attr_name)
+        if extracted_type_2:
+            return 'obj'
+        elif data_type.isalpha():
+            return 'str'
+        elif data_type == "'":
+            return 'str'
+        elif data_type == '{':
+            return 'dict'
+        elif data_type == '[':
+            return 'list'
+        elif data_type == '(':
+            return 'tuple'
+        elif data_type.isdigit():
+            return "int"
+        else:
+            print("No data type detected for '{0}'".format(attr_name))
+
+    def get_attribute_dictionary(self):
+        return self.def_data_types_dict
