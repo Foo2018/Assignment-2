15da2e95bc9466a89600e05fc24bcdc6e4a22f24
Foo2018 <chriswhiting6868@live.com>
2018-09-17 00:10:20 +1200

    Tests amended and reduced as many redundant test prior
    Re-evaluated unit testing and removed many unwanted unit tests. Will now test to 100% coverage standard all versions of extractor created thus far

--- test_extractor.py
+++ test_extractor.py
@@ -1,11 +1,11 @@
 import unittest
 import sys
 from extractor import Extractor
-from textsearch import TextSearch
+from attribute_default_search import AttributeDefaultsSearch
 import io
 from unittest import mock
 
-""" 33 unit tests to test the extractor.py class providing 100% coverage """
+""" 20 unit tests to test the extractor.py class providing 100% coverage """
 
 
 class MainTests(unittest.TestCase):
@@ -13,7 +13,7 @@ class MainTests(unittest.TestCase):
 
     def setUp(self):
         self.e = Extractor()
-        self.t = TextSearch()
+        self.t = AttributeDefaultsSearch()
 
     def test_unknown_file_entered(self):
         """Returns error if unknown file entered"""
@@ -55,56 +55,15 @@ class MainTests(unittest.TestCase):
         patched.assert_called_with('Folder1\\folder2\\folder3\\folder 4\\folder 5'
                                    '\\TestFile.py')
 
-    def test_class_extraction(self):
-        """A class name can be extracted from line of code"""
-        # Arrange
-        expected = 'Marsupial'
-        line = "class Marsupial(Mammal):"
-        # Act
-        actual = self.t._extract_class(line)
-        # Assert
-        self.assertEqual([expected], actual)
-
-    def test_class_extraction_without_dependency(self):
-        """A class name can be extracted from line of code where no parameters
-         are set i.e. Python3 style"""
-        # Arrange
-        expected = 'Marsupial'
-        line = "class Marsupial:"
-        # Act
-        actual = self.t._extract_class(line)
-        # Assert
-        self.assertEqual([expected], actual)
-
-    def test_nil_class_extraction(self):
-        """A name will not be extracted from line of code where no class present"""
-        # Arrange
-        expected = []
-        line = "class(Mammal):"
-        # Act
-        actual = self.t._extract_class(line)
-        # Assert
-        self.assertEqual(expected, actual)
-
     def test_function_extraction(self):
         """A function name can be extracted from line of code"""
         # Arrange
         expected = 'proliferates'
         line = "def proliferates(self):"
         # Act
-        actual = self.t._extract_functions(line)
+        actual = self.e._extract_functions(line)
         # Assert
         self.assertEqual([expected], actual)
-
-    def test_nil_function_extraction(self):
-        """A name will not be extracted from line of code where no function present"""
-        # Arrange
-        expected = []
-        line = "    teeth = 'Sharp'"
-        # Act
-        actual = self.t._extract_class(line)
-        # Assert
-        self.assertEqual(expected, actual)
 
     def test_attribute_extraction(self):
         """An attribute name can be extracted from line of code"""
@@ -115,154 +74,6 @@ class MainTests(unittest.TestCase):
         actual = self.e._extract_attributes(line)
         # Assert
         self.assertEqual([expected], actual)
-
-    def test_extract_defaults_data_types(self):
-        """Tests the _extract_defaults_data_types function returns a dictionary when true"""
-        # Arrange
-        expected = "{'obj': 'Extractor()'}"
-        line = "    self.e = Extractor()"
-        # Act
-        actual = self.e._extract_defaults_data_types(line)
-        # Assert
-        self.assertEqual(str(actual), expected)
-
-    def test_extract_defaults_data_types_no_extraction(self):
-        """Tests the _extract_defaults_data_types function returns a dictionary when true"""
-        # Arrange
-        expected = ''
-        line = ""
-        # Act
-        actual = self.e._extract_defaults_data_types(line)
-        # Assert
-        self.assertEqual(actual, expected)
-
-    def test_nil_attribute_extraction(self):
-        """A name will not be extracted from line of code where no attribute present"""
-        # Arrange
-        expected = []
-        line = "    self.= 'Sharp'"
-        # Act
-        actual = self.t._extract_class(line)
-        # Assert
-        self.assertEqual(expected, actual)
-
-    def test_default_value_extraction(self):
-        """An attribute default value can be extracted from line of code"""
-        # Arrange
-        expected = 'Sharp'
-        line = "    self.teeth = Sharp"
-        # Act
-        actual = self.e._extract_attribute_defaults(line)
-        # Assert
-        self.assertEqual(expected, actual)
-
-    def test_nil_default_value_extraction(self):
-        """A value will not be extracted from line of code where no default
-         present"""
-        # Arrange
-        expected = ''
-        line = "    self.teeth = "
-        # Act
-        actual = self.e._extract_attribute_defaults(line)
-        # Assert
-        self.assertEqual(expected, actual)
-
-    def test_data_type_object_extraction(self):
-        """An attribute data type can be extracted from line of code"""
-        # Arrange
-        expected = 'obj'
-        line = "Extractor()"
-        # Act
-        actual = self.e._extract_attribute_data_types(line)
-        # Assert
-        self.assertEqual(expected, actual)
-
-    def test_non_attribute_extraction(self):
-        """Handles data in __init__ that have are not attributes"""
-        # Arrange
-        expected = []
-        line = '  self.setup()'
-        # Act
-        actual = self.e._extract_attribute_defaults(line)
-        # Assert
-        self.assertEqual(expected, actual)
-
-    def test_data_type_string_extraction(self):
-        """An attribute data type can be extracted from line of code"""
-        # Arrange
-        expected = 'str'
-        line = "Sharp"
-        # Act
-        actual = self.e._extract_attribute_data_types(line)
-        # Assert
-        self.assertEqual(expected, actual)
-
-    def test_data_type_string_variation_extraction(self):
-        """An attribute data type can be extracted from line of code"""
-        # Arrange
-        expected = 'str'
-        line = "'"
-        # Act
-        actual = self.e._extract_attribute_data_types(line)
-        # Assert
-        self.assertEqual(expected, actual)
-
-    def test_data_type_dictionary_extraction(self):
-        """An attribute data type can be extracted from line of code"""
-        # Arrange
-        expected = 'dict'
-        line = "{"
-        # Act
-        actual = self.e._extract_attribute_data_types(line)
-        # Assert
-        self.assertEqual(expected, actual)
-
-    def test_data_type_list_extraction(self):
-        """An attribute data type can be extracted from line of code"""
-        # Arrange
-        expected = 'list'
-        line = "["
-        # Act
-        actual = self.e._extract_attribute_data_types(line)
-        # Assert
-        self.assertEqual(expected, actual)
-
-    def test_data_type_tuple_extraction(self):
-        """An attribute data type can be extracted from line of code"""
-        # Arrange
-        expected = 'tuple'
-        line = "("
-        # Act
-        actual = self.e._extract_attribute_data_types(line)
-        # Assert
-        self.assertEqual(expected, actual)
-
-    def test_data_type_integer_extraction(self):
-        """An attribute data type can be extracted from line of code"""
-        # Arrange
-        expected = 'int'
-        line = '2'
-        # Act
-        actual = self.e._extract_attribute_data_types(line)
-        # Assert
-        self.assertEqual(expected, actual)
-
-    def test_unknown_data_type_extraction(self):
-        """An error message is raised when an unknown attribute data type is extracted from line of code"""
-        # Arrange
-        line = '@'
-        # Assert
-        saved_stdout = sys.stdout
-        # Act
-        try:
-            out = io.StringIO()
-            sys.stdout = out
-            self.e._extract_attribute_data_types(line)
-            output = out.getvalue()
-            # assert
-            self.assertEqual(output, "No data type detected for '@'\n")
-        finally:
-            sys.stdout = saved_stdout
 
     def test_single_dependency_extraction(self):
         """Extracts a single object that class inherits from"""
