196a9a7ab05631f1af57aa1cb85afbc5d39d31e9
Foo2018 <chriswhiting6868@live.com>
2018-09-18 11:55:24 +1200

    Bad smell - duplicate code

---
+++ search_abstract.py
@@ -0,0 +1,20 @@
+from abc import ABC
+from abc import abstractmethod
+
+
+class ExtractionAbstract(ABC):
+
+    def __init__(self):
+        self.id = None
+
+    @abstractmethod
+    def set_file(self, file_path):
+        pass
+
+    @abstractmethod
+    def _data_extraction(self, file_path):
+        pass
+
+    @abstractmethod
+    def get_component_dictionary(self):
+        pass
