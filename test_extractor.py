fa1ec9954754f77a9f4f83843003152a6bd16883
Foo2018 <chriswhiting6868@live.com>
2018-09-17 19:24:57 +1200

    Major changes to unit tests that now allow true 100% coverage no matter the extractor version that is input
    Also tests the three classes used by Extractor

--- test_extractor.py
+++ test_extractor.py
@@ -13,7 +13,6 @@ class MainTests(unittest.TestCase):
 
     def setUp(self):
         self.e = Extractor()
-        self.t = AttributeDefaultsSearch()
 
     def test_unknown_file_entered(self):
         """Returns error if unknown file entered"""
@@ -27,9 +26,28 @@ class MainTests(unittest.TestCase):
             output = out.getvalue()
             # assert
             self.assertEqual(output, "File or directory not found\n")
-
         finally:
             sys.stdout = saved_stdout
+
+    def test_master(self):
+        "Tests the happy day scenario of an error free file being presented"
+        expected = True
+        self.e.set_file("mammals.py")
+        if len(self.e.component_dict) > 0:
+            actual = True
+        else:
+            actual = False
+        self.assertEqual(expected, actual)
+
+    def test_master_error(self):
+        "Tests the bad day scenario of an error file being presented"
+        expected = True
+        self.e.set_file("mammals_attribute_error.py")
+        if len(self.e.component_dict) > 0:
+            actual = True
+        else:
+            actual = False
+        self.assertEqual(expected, actual)
 
     def test_file_entered(self):
         """ Tests if a correct file is entered that the _data_extraction method is called"""
@@ -55,85 +73,9 @@ class MainTests(unittest.TestCase):
         patched.assert_called_with('Folder1\\folder2\\folder3\\folder 4\\folder 5'
                                    '\\TestFile.py')
 
-    def test_function_extraction(self):
-        """A function name can be extracted from line of code"""
-        # Arrange
-        expected = 'proliferates'
-        line = "def proliferates(self):"
-        # Act
-        actual = self.e._extract_functions(line)
-        # Assert
-        self.assertEqual([expected], actual)
-
-    def test_attribute_extraction(self):
-        """An attribute name can be extracted from line of code"""
-        # Arrange
-        expected = 'teeth'
-        line = "    self.teeth = 'Sharp'"
-        # Act
-        actual = self.e._extract_attributes(line)
-        # Assert
-        self.assertEqual([expected], actual)
-
-    def test_single_dependency_extraction(self):
-        """Extracts a single object that class inherits from"""
-        # Arrange
-        expected = ["Test1"]
-        line = 'class Example(Test1)'
-        # Act
-        actual = self.e._extract_parents(line)
-        # Assert
-        self.assertEqual(expected, actual)
-
-    def test_object_only_dependency_(self):
-        """Does not extract 'object' if it is only dependency"""
-        # Arrange
-        expected = []
-        line = 'class Example(object)'
-        # Act
-        actual = self.e._extract_parents(line)
-        # Assert
-        self.assertEqual(expected, actual)
-
-    def test_multiple_dependency_extraction(self):
-        # Arrange
-        expected = ["Test1", "Test2", "Test3"]
-        line = 'class Example(Test1, Test2, Test3)'
-        # Act
-        actual = self.e._extract_parents(line)
-        # Assert
-        self.assertEqual(expected, actual)
-
-    def test_no_class_defined_function_exception(self):
-        """An error message is raised when an function is defined before its class
-        due to poor coding"""
-        # Arrange Act Assert
-        self.assertRaises(Exception, self.e._data_extraction, "errorTestFunctions.py")
-
-    def test_no_class_defined_attribute_exception(self):
-        """An error message is raised when an attribute is defined before its class
-        due to poor coding"""
-        # Arrange Act Assert
-        self.assertRaises(Exception, self.e._data_extraction, "errorTestAttributes.py")
-
-    def test_class_name(self):
-        expected = True
-        self.e._data_extraction("mammals.py")
-        if self.e.component_dict:
-            actual = True
-        else:
-            actual = False
-        self.assertEqual(actual, expected)
-
-    def test_extractor_class_get_function(self):
-        """Ensure that getter can retrieve a dictionary"""
-        self.e._data_extraction("mammals.py")
-        dictionary = self.e.get_component_dictionary()
-        self.assertTrue(dictionary)
-
     def test_stub_creation(self):
         expected = True
-        self.e._data_extraction("example.py")
+        self.e.set_file("example.py")
         if self.e.component_dict:
             actual = True
         else:
@@ -142,24 +84,30 @@ class MainTests(unittest.TestCase):
 
     def test_attribute_extraction_when_no_init(self):
         expected = True
-        self.e._data_extraction("NoInit.py")
-        if self.e.component_dict:
+        self.e.set_file("no_init.py")
+        if len(self.e.component_dict) > 0:
             actual = True
         else:
             actual = False
-        self.assertEqual(actual, expected)
+        self.assertEqual(expected, actual)
 
-    def test_attribute_declared(self):
-        # Arrange
-        expected = True
-        # Act
-        self.e._data_extraction("component.py")
-        if self.e.component_dict:
-            actual = True
-        else:
-            actual = False
-        # Assert
-        self.assertEqual(actual, expected)
+    def test_no_class_defined_function_exception(self):
+        """An error message is raised when an function is defined before its class
+        due to poor coding"""
+        # Arrange Act Assert
+        self.assertRaises(Exception, self.e.set_file, "errorTestFunctions.py")
+
+    def test_no_class_defined_attribute_exception(self):
+        """An error message is raised when an attribute is defined before its class
+        due to poor coding"""
+        # Arrange Act Assert
+        self.assertRaises(Exception, self.e.set_file, "errorTestAttributes.py")
+
+    def test_extractor_class_get_function(self):
+        """Ensure that getter can retrieve a dictionary"""
+        self.e.set_file("mammals.py")
+        dictionary = self.e.get_component_dictionary()
+        self.assertTrue(dictionary)
 
 
 if __name__ == '__main__':
