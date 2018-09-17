178bf4af92acb19244da9c282e05a8b733ef35aa
Foo2018 <chriswhiting6868@live.com>
2018-09-17 19:19:13 +1200

    Removal of abtract methods that are no longer implemented in concrete Extractor class

--- extraction_abstract.py
+++ extraction_abstract.py
@@ -16,31 +16,6 @@ class ExtractionAbstract(ABC):
     def _data_extraction(self, file_path):
         pass
 
-    @staticmethod
-    @abstractmethod
-    def _regex_search(regex, data):
-        pass
-
-    @abstractmethod
-    def _extract_parents(self, line):
-        pass
-
-    @abstractmethod
-    def _extract_attributes(self, line):
-        pass
-
-    @abstractmethod
-    def _extract_defaults_data_types(self, line):
-        pass
-
-    @abstractmethod
-    def _extract_attribute_defaults(self, line):
-        pass
-
-    @abstractmethod
-    def _extract_attribute_data_types(self, attr_name):
-        pass
-
     @abstractmethod
     def get_component_dictionary(self):
         pass
